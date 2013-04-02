fab690b6a3180ea7f3e609921fd3edcb22f1438d
Paul Laufer <paul@laufer.at>
2013-04-02 18:38:15 +0200

    fixed mockup parser for sax

--- parser/reader.py
+++ parser/reader.py
@@ -6,11 +6,25 @@ class Reader(xml.sax.handler.ContentHandler):
     def __init__(self, target_attrs=()):
         self.target_attrs = target_attrs
         self.counter = 0
+	self._current_title = u''
+        self._in_title = False
 
     def startElement(self, name, attrs):
         if name == 'page':
-            print attrs.get('title', u'')
             self.counter = self.counter + 1
+        elif name == 'title':
+            self._in_title = True 
+
+    def characters(self, content):
+        if self._in_title:
+            self._current_title += content
+
+    def endElement(self, name):
+        if name == 'title':
+            print '%08d: %r' % (self.counter, self._current_title)
+            self._in_title = False
+            self._current_title = u''
+
 
 if __name__ == '__main__':
     time.clock()
