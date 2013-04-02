223183dab6949af82b1669e529a09fb313fb171a
Paul Laufer <paul@laufer.at>
2013-04-02 18:26:18 +0200

    tried sax version

--- parser/reader.py
+++ parser/reader.py
@@ -1,38 +1,20 @@
 import time
-from lxml import etree
+import xml.sax
 
-# Parts of this code are taken from
-# http://www.ibm.com/developerworks/xml/library/x-hiperfparse/
-# Author: Liza Daly
+class Reader(xml.sax.handler.ContentHandler):
+    """SAX Handler which will store selected attribute values."""
+    def __init__(self, target_attrs=()):
+        self.target_attrs = target_attrs
+        self.counter = 0
 
-class Reader:
-    def __init__(self, wikipedia_file):
-        self._wikipedia_file = wikipedia_file #open(wikipedia_file, 'r')
-        self._counter = 0
-
-    def __del__(self):
-        self._wikipedia_file.close()
-
-    def process_article(self, elem):
-        print elem.xpath( 'title/text( )' )
-        self._counter = self._counter + 1
-
-    def read_articles(self, context):
-        for event, elem in context:
-            self._process_article(elem)
-            elem.clear()
-            del elem
-            while elem.getprevious() is not None:
-                del elem.getparent()[0]
-        del context
-
-    def read(self):
-        context = etree.iterparse(self._wikipedia_file, tag='page')
-        self.read_articles(context)
-	print self._counter
+    def startElement(self, name, attrs):
+        if name == 'page':
+            print attrs.get('title', u'')
+            self.counter = self.counter + 1
 
 if __name__ == '__main__':
     time.clock()
-    reader = Reader('/home/paul/data/wikipedia/enwiki-20130102-pages-articles.xml')
-    reader.read()
+    grabber = Reader(target_attrs=('page'))
+    xml.sax.parse('/home/paul/data/wikipedia/enwiki-20130102-pages-articles.xml', grabber)
     print time.clock()
+    print grabber.counter
