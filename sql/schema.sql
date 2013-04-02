4bc475d42c63059a426d65d97e4b445e164bb9fc
Paul Laufer <paul@laufer.at>
2013-04-02 17:49:30 +0200

    added sql schema for mysql

---
+++ sql/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `articles` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `lastparsed` DATETIME NOT NULL,
+    `title` VARCHAR(200) NOT NULL UNIQUE,
+    `linkincount` INTEGER NOT NULL DEFAULT 0,
+    `linkoutcount` INTEGER NOT NULL DEFAULT 0,
+    INDEX USING HASH (`title`)
+);
+
+CREATE TABLE `links` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `article_id` INTEGER NOT NULL,
+    `token_index` INTEGER NOT NULL,
+    `target_article` VARCHAR(200) NOT NULL,
+    FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`),
+    INDEX (`article_id`)
+);
+
+CREATE TABLE `disambiguations` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `string` VARCHAR(200) NOT NULL,
+    `meaning` VARCHAR(200) NOT NULL,
+    `article_id` INTEGER NOT NULL,
+    `token_index` INTEGER NOT NULL,
+    FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`),
+    INDEX (`article_id`),
+    INDEX USING HASH (`string`)
+);
