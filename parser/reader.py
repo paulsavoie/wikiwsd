c379dca2508d177be6554fc53ec96de2f8d2864d
Paul Laufer <paul@laufer.at>
2013-04-02 18:22:10 +0200

    lxml fixed but not efficient

--- parser/reader.py
+++ parser/reader.py
@@ -1,3 +1,4 @@
+import time
 from lxml import etree
 
 # Parts of this code are taken from
@@ -6,7 +7,7 @@ from lxml import etree
 
 class Reader:
     def __init__(self, wikipedia_file):
-        self._wikipedia_file = open(wikipedia_file, 'r')
+        self._wikipedia_file = wikipedia_file #open(wikipedia_file, 'r')
         self._counter = 0
 
     def __del__(self):
@@ -16,17 +17,19 @@ class Reader:
         print elem.xpath( 'title/text( )' )
         self._counter = self._counter + 1
 
-    def read_article(self, context):
+    def read_articles(self, context):
         for event, elem in context:
             self._process_article(elem)
             elem.clear()
+            del elem
             while elem.getprevious() is not None:
                 del elem.getparent()[0]
         del context
 
     def read(self):
         context = etree.iterparse(self._wikipedia_file, tag='page')
-        self.process_article(context)
+        self.read_articles(context)
+	print self._counter
 
 if __name__ == '__main__':
     time.clock()
