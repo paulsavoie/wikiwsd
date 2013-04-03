b166dc612426b506e56a309afc5fe48546c57800
Paul Laufer <paul@laufer.at>
2013-04-03 10:26:32 +0200

    modified reader to read all important fields

--- parser/reader.py
+++ parser/reader.py
@@ -3,32 +3,42 @@ import xml.sax
 
 class Reader(xml.sax.handler.ContentHandler):
     """SAX Handler which will store selected attribute values."""
-    def __init__(self, target_attrs=()):
-        self.target_attrs = target_attrs
+    def __init__(self):
         self.counter = 0
-	self._current_title = u''
-        self._in_title = False
+        self._reset_article()
+        self._current_tag = u''
+
+    def _reset_article(self):
+        self._article = {
+            'title': u'',
+            'id': u'',
+            'text': u''
+        }
 
     def startElement(self, name, attrs):
-        if name == 'page':
-            self.counter = self.counter + 1
-        elif name == 'title':
-            self._in_title = True 
+        self._current_tag = name
 
     def characters(self, content):
-        if self._in_title:
-            self._current_title += content
+        if self._current_tag == 'title':
+            self._article['title'] += content
+        elif self._current_tag == 'id':
+            self._article['id'] += content
+        elif self._current_tag == 'text':
+            self._article['text'] += content
 
     def endElement(self, name):
-        if name == 'title':
-            print '%08d: %r' % (self.counter, self._current_title)
-            self._in_title = False
-            self._current_title = u''
+        self._current_tag = u''
+        if name == 'page':
+            if self._article['text'][:len('#REDIRECT')] != '#REDIRECT':
+                print '%08d: %r' % (self.counter, self._article['title'])
+                self._current_tag = u''
+                self.counter = self.counter + 1
+            self._reset_article()
 
 
 if __name__ == '__main__':
     time.clock()
-    grabber = Reader(target_attrs=('page'))
-    xml.sax.parse('../data/training.xml', grabber)
+    reader = Reader()
+    xml.sax.parse('../data/training.xml', reader)
     print time.clock()
-    print grabber.counter
+    print reader.counter
