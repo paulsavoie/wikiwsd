ca20379017bdd51cfa3e307de247979bd672fbc6
Paul Laufer <paul@laufer.at>
2013-05-02 08:04:09 +0200

    added last changes

--- updater/updatethread.py
+++ updater/updatethread.py
@@ -4,6 +4,7 @@
 import time
 import threading
 import Queue
+import MySQLdb as mysqldb
 
 class UpdateThread(threading.Thread):
     def __init__(self, link_queue, db_connection=None):
@@ -16,12 +17,15 @@ class UpdateThread(threading.Thread):
         while not self._end:
             try:
                 title = self._queue.get(True, 2)
-                print 'updating article "%s"' % title
+                #print 'updating article "%s"' % title
                 cur = self._db_connection.cursor()
-                cur.execute('UPDATE articles SET linkincount = linkincount + 1 WHERE title="%s"' % (title))
+                cur.execute('UPDATE articles SET linkincount = linkincount + 1 WHERE title=%s;', (title))
                 self._db_connection.commit()
             except Queue.Empty:
                 pass
+            except mysqldb.Error, e:
+                print "Error in article '%s'" % (title.encode('ascii', 'ignore'))
+                print "Error %d: %s" % (e.args[0],e.args[1])
 
     def end(self):
         self._end = True
