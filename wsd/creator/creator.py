93bf6606acc47ba2c78943d747ba5b3692fe4c99
Paul Laufer <paul@laufer.at>
2013-07-01 11:03:24 +0200

    added comments to creator classes

--- wsd/creator/creator.py
+++ wsd/creator/creator.py
@@ -1,5 +1,12 @@
-"""The main program
-"""
+# -*- coding: utf-8 -*-
+'''
+This file contains the code to initiate the building
+process of the database
+
+Author: Paul Laufer
+Date: Jun 2013
+
+'''
 
 import time
 import Queue
@@ -10,6 +17,19 @@ from preparation import ResolveThread
 from preparation import PrepareThread
 
 class Creator():
+    ''' class that handles the building process of the database
+    '''
+
+    '''constructor
+
+    Arguments:
+        xml_path --- the path to the wikipedia dump file
+        max_queue-size --- the maximum number of articles to hold in memory at any time (default to 20)
+        num_threads --- the number of processing threads to use (defaults to 1)
+        db_host --- the host name of the database (default to 'localhost')
+        db_port --- the port number of the database server (defaults to 27017)
+        action --- the action to perform, either 'prepare' or 'learn' (defaults to 'learn')
+    '''
     def __init__(self, xml_path, max_queue_size=20, num_threads=1, 
             db_host='localhost', db_port=27017, action='learn'):
         self._queue = Queue.Queue(maxsize=max_queue_size)
@@ -27,6 +47,8 @@ class Creator():
                 #client = MongoClient(db_host, db_port)
                 self._worker_threads.append(PrepareThread(self._queue, client, 'wikiwsd'))
 
+    '''starts the building process of the database
+    '''
     def run(self):
         self._reading_thread.start()
         for worker in self._worker_threads:
@@ -40,16 +62,3 @@ class Creator():
             worker.end()
         for worker in self._worker_threads:
             worker.join()
-
-
-if __name__ == '__main__':
-    # first action 'prepare' to learn links
-    # then action 'learn' to learn
-    #prog = Creator('/home/paul/data/wikipedia/enwiki-20130102-pages-articles.xml', num_threads=26, max_queue_size=300, action='prepare')
-    #prog = Creator('/home/paul/data/wikipedia/enwiki-20130102-pages-articles.xml', num_threads=26, max_queue_size=300, action='learn')
-
-    #prog = Creator('../data/training.xml', num_threads=8, max_queue_size=300, db_host='localhost', action='prepare')
-    prog = Creator('../data/training.xml', num_threads=26, max_queue_size=300, db_host='localhost', action='learn')
-    time.clock()
-    prog.run()
-    print time.clock()
--- wsd/creator/dbsetup.py
+++ wsd/creator/dbsetup.py
@@ -1,10 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+This file contains the code to setup the
+mongodb database
+
+Author: Paul Laufer
+Date: Jun 2013
+
+'''
+
 import pymongo
 
 class DBSetup():
+    '''class to setup the database
+    '''
+
+    '''constructor
+
+    Arguments:
+        host --- the host name of the mongodb installation
+        port --- the port of the server
+    '''
     def __init__(self, host, port):
         self.__host = host
         self.__port = port
 
+    '''prepares the database
+    '''
     def run(self):
         client = pymongo.MongoClient(self.__host, self.__port)
         db = client.wikiwsd
