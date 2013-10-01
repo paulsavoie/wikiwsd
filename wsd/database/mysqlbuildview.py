6119a456b5b287ea1315878f915bff89134eaeb2
Paul Laufer <paul@laufer.at>
2013-10-01 16:50:47 +0200

    added mysqldatabase and mysqlbuildview without tests

---
+++ wsd/database/mysqlbuildview.py
@@ -0,0 +1,125 @@
+import MySQLdb
+import logging
+
+class MySQLBuildView:
+    """The MySQLBuildView class allows database access optimized to
+       build the disambiguation database
+    """
+
+    def __init__(self, db_connection):
+        """constructor
+
+           @param db_connector the database connector used to access the database
+        """
+        self._db_connection = db_connection
+        self._cursor = db_connection.cursor()
+        self.reset_cache()
+
+    def __del__(self):
+        """destructor
+           closes the database connection
+        """
+        self._db_connection.close()
+
+    def insert_article(self, id, title):
+        """saves an article in the database
+
+           @param id the id of the article
+           @param title the title of the article 
+        """
+        try:
+            self._cursor.execute('INSERT INTO articles(id, title) VALUES(%s, %s);', 
+                id, title)
+        except MySQLdb.Error, e:
+            logging.error('error saving article "%s" to database: %s (%d)' 
+                % (title.encode('ascii', 'ignore'), e.args[1], e.args[0]))
+
+    def insert_redirect(self, source_name, target_name):
+        """saves a redirect in the database
+
+        @param source_name the name of the source article
+        @param target_name the name of the target article
+        """
+        try:
+            self._cursor.execute('INSERT INTO redirects(source_article_name, target_article_name) VALUES(%s, %s);',
+                source_name, target_name)
+        except MySQLdb.Error, e:
+            logging.error('error saving redirect "%s" --> "%s" to database: %s (%d)' 
+                % (source_name.encode('ascii', 'ignore'), target_name.encode('ascii', 'ignore'), e.args[1], e.args[0]))
+
+    def insert_link(self, source_article_id, target_article_name):
+        """saves a link to the database and updates the article record it points to
+
+           @param source_article_id the id of the article which links to the target
+           @param target_article_name the name of the target article
+        """
+        target_article_id = self._resolve_title(target_article_name)
+        if target_article_id == None:
+            logging.error('Could not resolve target article "%s" for link from source article %d' 
+                % (target_article_name.encode('ascii', 'ignore'), source_article_id))
+        else:
+            try:
+                self._cursor.execute('INSERT INTO links(source_article_id, target_article_id) VALUES(%s, %s);',
+                    source_article_id, target_article_id)
+            except MySQLdb.Error, e:
+                logging.error('error saving link (%d) --> (%d) to database: %s (%d)' 
+                % (source_article_id, target_article_id, e.args[1], e.args[0]))
+
+    def insert_disambiguation(self, string, target_article_name):
+        """saves a disambiguation to the database
+
+           @param string the disambiguation string used for the linked entity
+           @param target_article_name the name of the article the disambiguation stands for
+        """
+        target_article_id = self._resolve_title(target_article_name)
+        if target_article_id == None:
+            logging.error('Could not resolve target article "%s" for link from source article %d' 
+                % (target_article_name.encode('ascii', 'ignore'), source_article_id))
+        else:
+            try:
+                self._cursor.execute('INSERT INTO disambiguations(string, target_article_id, occurrences) VALUES(%s,  %s, 1) ON DUPLICATE KEY UPDATE occurrences=occurrences+1;',
+                    string, target_article_id)
+            except MySQLdb.Error, e:
+                logging.error('error saving disambiguation "%s" --> %s (%d): %s (%d)'
+                    % (string.encode('ascii', 'ignore'), target_article_name.encode('ascii', 'ignore'), target_article_id, e.args[1], e.args[0]))
+
+    def insert_ngrams(self, ngrams):
+        """inserts ngrams into the database
+
+           @param ngrams a list of ngrams where each ngram is a tuple containing the string,
+                         and a zero or one indicating whether it was used as a link
+        """
+        try:
+            self._cursor.executemany('INSERT INTO ngrams(string, occurrences, as_link) VALUES(%s, 1, %s) ON DUPLICATE KEY UPDATE occurrences=occurrences+1, as_link=as_link+VALUES(as_link);',
+                    ngrams)
+        except MySQLdb.error, e:
+            logging.error('error saving ngrams: %s (%d)' % (e.args[1], e.args[0]))
+
+    def reset_cache(self):
+        """resets the internal cache and thus prevents it from growing too big
+        """
+        self._article_id_cache = {}
+
+    def _resolve_title(self, title):
+        """resolves an article and returns its id
+
+           @param title the title of the article
+        """
+        if title in self._article_id_cache:
+            return self._article_id_cache[title]
+
+        try:
+            self._cursor.execute('SELECT id FROM articles WHERE title=%s;', title)
+            row = self._cursor.fetchone()
+            if row == None:
+                cur.execute('SELECT id FROM articles WHERE title=(SELECT target_article_name FROM redirects WHERE source_article_name=%s);',
+                        title)
+                row = self.cursor.fetchone()
+
+            if row == None:
+                self._article_id_cache[title] = None
+            else:
+                self._article_id_cache[title] = row[0]
+
+        return self._article_id_cache[title]
+
---
+++ wsd/database/mysqldatabase.py
@@ -0,0 +1,133 @@
+import MySQLdb as mysqldb
+import logging
+import time
+import MySQLBuildView
+
+class MySQLDatabase:
+    """The MySQLDatabase class allows to build and manage the
+       access to a mysql database
+    """
+
+    def __init__(self, host='localhost', user='wikiwsd', passwd='wikiwsd', database='wikiwsd3'):
+        """constructor
+
+           @param host the host to connect to
+           @param user the database user
+           @param passwd the password to be used in the connection
+           @param database the database to select on the server
+        """
+        self._host = host
+        self._user = user
+        self._passwd = passwd
+        self._database = database
+
+    def build(self):
+        """creates up the basic database structure
+        """
+        logging.info('Building database structure. Opening database connection...')
+        connection = self._create_connection()
+        cursor = connection.cursor()
+        letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'other')
+        start = time.clock()
+
+        # build articles table
+        logging.info('Building table articles...')
+        cursor.execute('CREATE TABLE `articles` ('
+                           '`id` BIGINT UNSIGNED NOT NULL PRIMARY KEY,'
+                           '`title` VARCHAR(200) NOT NULL,'
+                           '`articleincount` INTEGER NOT NULL DEFAULT 0,'
+                           'INDEX USING HASH(`title`)'
+                       ') ENGINE=InnoDB;')
+
+        # build redirects table
+        logging.info('Building table redirects...')
+        cursor.execute('CREATE TABLE `redirects` ('
+                           '`id` BIGINT UNSIGNED INT NOT NULL PRIMARY KEY AUTO_INCREMENT,'
+                           '`source_article_name` VARCHAR(200) NOT NULL',
+                           '`target_article_name` VARCHAR(200) NOT NULL',
+                           'INDEX USING HASH(`source_article_name`)'
+                       ') ENGINE = InnoDB;')
+
+        # build links table
+        logging.info('Building table links...')
+        cursor.execute('CREATE TABLE `links` ('
+                           '`id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,'
+                           '`source_article_id` BIGINT UNSIGNED NOT NULL,'
+                           '`target_article_id` BIGINT UNSIGNED NOT NULL,'
+                           'INDEX(`target_article_id`)'
+                       ') ENGINE=InnoDB;')
+
+        # build disambiguations table
+        logging.info('Building table disambiguations...')
+        cursor.execute('CREATE TABLE `disambiguations` ('
+                           '`id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,'
+                           '`string` VARCHAR(200) NOT NULL,'
+                           '`target_article_id` BIGINT UNSIGNED NOT NULL,'
+                           '`occurrences` BIGINT UNSIGNED NOT NULL DEFAULT 0,'
+                           'INDEX USING HASH(`string`, `target_article_id`)'
+                       ') ENGINE=InnoDB;')
+
+        # build ngrams table
+        logging.info('Building table ngrams...')
+        cursor.execute('CREATE TABLE `ngrams` ('
+                           '`id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,'
+                           '`string` VARCHAR(200) NOT NULL,'
+                           '`occurrences` INT UNSIGNED NOT NULL DEFAULT 0,'
+                           '`as_link` INT UNSIGNED NOT NULL DEFAULT 0,'
+                           'INDEX USING HASH(`string`)'
+                       ') ENGINE=InnoDB;')
+
+        logging.info('closing database connection...')
+        connection.close()
+
+        end = time.clock()
+        logging.info('DONE building tables (took %f seconds)' % (end-start))
+
+    
+    def optimize(self):
+        """optimizes the database for query access
+        """
+        logging.info('Building database structure. Opening database connection...')
+        connection = self._create_connection()
+        cursor = connection.cursor()
+        start = time.clock()
+        
+        logging.info('optimizing table articles...')
+        cursor.execute('OPTIMIZE TABLE articles;')
+
+        logging.info('optimizing table redirects...')
+        cursor.execute('OPTIMIZE TABLE redirects;')
+
+        logging.info('optimizing table links...')
+        cursor.execute('OPTIMIZE TABLE links;')
+
+        logging.info('optimizing table disambiguations...')
+        cursor.execute('OPTIMIZE TABLE disambiguations;')
+
+        logging.info('optimizing table ngrams...')
+        cursor.execute('OPTIMIZE TABLE ngrams;')
+
+        logging.info('closing database connection...')
+        connection.close()
+
+        end = time.clock()
+        total = round(end-start)
+        minutes = total / 60
+        secs = total % 60
+        logging.info('DONE optimizing tables (took %d minutes and %d seconds)' % (minutes, seconds))
+
+    def get_build_view(self):
+        """returns a new database view optimized for the build process
+        """
+        connection = self._create_connection()
+        view = MySQLBuildView(connection)
+        return view
+
+    def get_work_view(self):
+        """returns a new database view optimized for the disambiguation process
+        """
+        pass
+
+    def _create_connection(self):
+        con = mysqldb.connect(self._host, self._user, self._passwd, self._database, charset='utf8', use_unicode=True)
+        return con
